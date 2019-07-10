ac1a26f405d55115a7d876ff2f8cf390e4d0a641
bear <591566564@qq.com>
2019-07-10 10:13:42 +0800

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #安装filebeat
 mkdir ~/soft
 cd ~/soft
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.5.4-x86_64.rpm
+wget --no-check-certificate https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.5.4-x86_64.rpm
 yum install filebeat-6.5.4-x86_64.rpm -y
 
 #配置开机自启动
@@ -11,10 +11,12 @@ echo 'systemctl start filebeat' >> /etc/rc.d/rc.local
 
 #修改配置文件
 mv /etc/filebeat/filebeat.yml  /etc/filebeat/filebeat-copy.yml
-mv filebeat.yml /etc/filebeat/filebeat.yml
+cp /home/filebeat_install/filebeat.yml /etc/filebeat/filebeat.yml
 
 #启动filebeat
 systemctl start filebeat
+
+sleep 10s
 
 #查询启动日志
 tail -f /var/log/filebeat/filebeat
